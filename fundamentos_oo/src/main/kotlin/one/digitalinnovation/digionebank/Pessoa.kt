54316b51787605c7aab56c74894b19ac7850b176
LucasMoraesMarques <lucasmoraes7551@gmail.com>
2021-06-30 10:41:40 -0300

    updated to talk about class members

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,9 +4,16 @@ class Pessoa {
     var name: String = "Lucas"
     var cpf: String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$name e $cpf"
+
+
 }
 
 fun main(){
     var lucas = Pessoa()
+    println(lucas.pessoaInfo())
 
 }
